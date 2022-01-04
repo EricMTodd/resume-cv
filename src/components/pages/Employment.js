006d0d54e095215e9d76dbc727c8f90bd12a0c52
ericmtodd <eterictodd@gmail.com>
2022-01-03 19:30:38 -0700

    Refined CSS by adding flex wrap to some containers.

--- src/components/pages/Employment.js
+++ src/components/pages/Employment.js
@@ -4,20 +4,16 @@ const Employment = () => {
       <h1>Employment</h1>
       <hr />
       <div className="employment-container">
-        <div>
-          <h2>Screen Printer - Harrow Sports</h2>
-          <h2>Driver - Luna Freight</h2>
-          <h2>Ruby on Rails Developer Intern - ExecOnline</h2>
-          <h2>Assisstant Warehouse Manager - Scandinavian Designs</h2>
-          <h2>Warehouse Associate - Ryder</h2>
-        </div>
-        <div>
-          <h2>Press Assisstant - Denver Screen Print and Emroidery</h2>
-          <h2>Driver - Lyft</h2>
-          <h2>Press Assisstant - Superior Ink</h2>
-          <h2>Warehouse Associate - First Nation Group</h2>
-          <h2>Shipping and Receiving - Namify</h2>
-        </div>
+        <h2>Screen Printer - Harrow Sports</h2>
+        <h2>Driver - Luna Freight</h2>
+        <h2>Ruby on Rails Developer Intern - ExecOnline</h2>
+        <h2>Assisstant Warehouse Manager - Scandinavian Designs</h2>
+        <h2>Warehouse Associate - Ryder</h2>
+        <h2>Press Assisstant - Denver Screen Print and Emroidery</h2>
+        <h2>Driver - Lyft</h2>
+        <h2>Press Assisstant - Superior Ink</h2>
+        <h2>Warehouse Associate - First Nation Group</h2>
+        <h2>Shipping and Receiving - Namify</h2>
       </div>
     </div>
   )
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -3,22 +3,16 @@ const Skills = () => {
     <div className="container bravo" id="skills">
       <h1>Skills</h1>
       <hr />
-      <div className="skills-container" id="skill-container-alpha">
+      <div className="skills-container">
         <h2>HTML</h2>
         <h2>CSS</h2>
         <h2>JavaScript</h2>
-      </div>
-      <div className="skills-container" id="skill-container-bravo">
         <h2>Rails</h2>
         <h2>Flexbox</h2>
         <h2>React</h2>
-      </div>
-      <div className="skills-container" id="skill-container-charlie">
         <h2>Graphic Design</h2>
         <h2>Print Production</h2>
         <h2>Screen Printing</h2>
-      </div>
-      <div className="skills-container" id="skill-container-delta">
         <h2>Linux</h2>
         <h2>Unix</h2>
         <h2>Windows</h2>
