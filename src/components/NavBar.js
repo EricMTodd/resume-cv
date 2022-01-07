6f24969d85176172f43327a1a30e5777b9405f91
ericmtodd <eterictodd@gmail.com>
2022-01-07 13:22:07 -0700

    Putting in work.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
 const NavBar = () => {
+
+  const toggleNav = () => {
+    console.log("Hello world!");
+  }
+
   return(
-    <nav>
-      <a href="#about">About</a> | 
-      <a href="#skills">Skills</a> | 
-      <a href="#projects">Projects</a> | 
-      <a href="#education">Education</a> | 
-      <a href="#employment">Employment</a> | 
-      <a href="#contact">Contact</a>
+    <nav className="collapsed-nav">
+      <button onClick={toggleNav}>Click me!</button>
+      <div className="nav-links">
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#education">Education</a>
+        <a href="#employment">Employment</a>
+        <a href="#contact">Contact</a>
+      </div>
     </nav>
   )
 }
