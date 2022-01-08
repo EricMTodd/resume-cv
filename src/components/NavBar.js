24a8ddacae5ca0a91fa046cb6a1bd3a73110ca1e
ericmtodd <eterictodd@gmail.com>
2022-01-08 13:30:57 -0700

    Navigation now works as intended.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 const NavBar = () => {
 
-  const toggleNav = (event) => {
+  const toggleNav = () => {
     const navLinks = document.querySelector("#nav-links");
-    console.log(navLinks);
+    const { body } = document;
     if (navLinks.className === "hidden-links") {
+      body.classList.add('restrict-scroll');
       navLinks.classList.remove("hidden-links");
       navLinks.classList.add("display-links");
     } else {
+      body.classList.remove('restrict-scroll');
       navLinks.classList.remove("display-links");
       navLinks.classList.add("hidden-links");
     }
@@ -18,12 +20,12 @@ const NavBar = () => {
         <button onClick={toggleNav} id="nav-button" className="hidden-button">X</button>
       </div>
       <div id="nav-links" className="hidden-links">
-        <a href="#about">About</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#education">Education</a>
-        <a href="#employment">Employment</a>
-        <a href="#contact">Contact</a>
+        <a href="#about" onClick={toggleNav}>About</a>
+        <a href="#skills" onClick={toggleNav}>Skills</a>
+        <a href="#projects" onClick={toggleNav}>Projects</a>
+        <a href="#education" onClick={toggleNav}>Education</a>
+        <a href="#employment" onClick={toggleNav}>Employment</a>
+        <a href="#contact" onClick={toggleNav}>Contact</a>
       </div>
     </nav>
   )
