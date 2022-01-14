fd19176c83f78a340157224877a8c53c735e4460
ericmtodd <eterictodd@gmail.com>
2022-01-13 19:17:18 -0700

    Added employment dates.

--- src/components/pages/Employment.js
+++ src/components/pages/Employment.js
@@ -5,16 +5,27 @@ const Employment = () => {
       <hr />
       <div className="employment-container">
         <h2>Screen Printer - Harrow Sports</h2>
+        <h4>October 2020 - Present</h4>
         <h2>Driver - Luna Freight</h2>
+        <h4>July 2020 - October 2020</h4>
         <h2>Ruby on Rails Developer Intern - ExecOnline</h2>
+        <h4>May 2020 - July 2020</h4>
         <h2>Assisstant Warehouse Manager - Scandinavian Designs</h2>
+        <h4>November 2019 - June 2020</h4>
         <h2>Warehouse Associate - Ryder</h2>
+        <h4>September 2019 - November 2019</h4>
         <h2>Press Assisstant - Denver Screen Print and Emroidery</h2>
+        <h4>April 2019 - September 2019</h4>
         <h2>Driver - Lyft</h2>
+        <h4>December 2018 - April 2019</h4>
         <h2>Press Assisstant - Superior Ink</h2>
+        <h4>September 2018 - December 2018</h4>
+        <h2>Student - General Assembly Web Development Immersive</h2>
+        <h4>June 2018 - September 2018</h4>
         <h2>Warehouse Associate - First Nation Group</h2>
+        <h4>January 2018 - June 2018</h4>
         <h2>Shipping and Receiving - Namify</h2>
-        <h2>Press Assisstant - Namify</h2>
+        <h4>March 2016 - November 2017</h4>
       </div>
     </div>
   )
