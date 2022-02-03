84c70e37c1c3208a51b08697fed38d1da0985535
ericmtodd <eterictodd@gmail.com>
2022-02-03 12:10:35 -0700

    Minor formatting changes.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,7 +5,9 @@ const Projects = () => {
       <hr />
       <div className="projects-container">
         <h2>Forum</h2>
-        <h2>Flickr</h2>
+        <a href='https://dashsplash.herokuapp.com' target='_blank'><h2>dashsplash</h2></a>
+        <i>Built with - React, openweathermap API and the Flickr API.</i>
+        <p>Dashsplash is a simple application that was based on lock screens and screen savers for many popular devices like Google's chromecast. If you share your location, it will show you the current weather for latitude and longitude and the city name you're currently in. If you block the location, it will show you the weather from a major city somewhere in the world and updates every fifteen minutes. The clock updates in real time, and the slideshow is served from Flickr, which changes images every minute.</p>
       </div>
     </div>
   )
